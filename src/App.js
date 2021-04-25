f91c4f7408914f0f0802e9a54f002cbf3ab40661
S <Sandhya.S@unisys.com>
2021-04-25 19:15:45 +0530

    timer-clock project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState("0:0");
+  const [startLable, setStartLable] = useState("Start");
+  const [timer, setTimer] = useState(null);
+
+  const lableAction = (la) => {
+    if (la == "Start") {
+      let hour = 0;
+      let min = 0;
+      setStartLable("Pause");
+      setTimer(
+        setInterval(() => {
+          min++;
+          if (min == 60) {
+            min = 0;
+            hour++;
+          }
+          setCount(`${hour}:${min}`);
+        }, 100)
+      );
+    } else if (la == "Stop") {
+      clearInterval(timer);
+      setTimer(null);
+      setCount(`0:0`);
+    } else if (la == "Pause") {
+      setStartLable("Resume");
+      clearInterval(timer);
+      setTimer(null);
+      setCount(count);
+    } else if (la == "Resume") {
+      let c = count.split(":");
+      let hour = c[0];
+      let min = c[1];
+      setStartLable("Pause");
+      setTimer(
+        setInterval(() => {
+          min++;
+          if (min == 60) {
+            min = 0;
+            hour++;
+          }
+          setCount(`${hour}:${min}`);
+        }, 100)
+      );
+    }
+  };
+
+  const startCount = () => {
+    lableAction(startLable);
+  };
+  const stopCount = () => {
+    lableAction("Stop");
+    setStartLable("Start");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="btn-container">
+        <button onClick={startCount}>{startLable}</button>
+        <button onClick={stopCount}>Stop</button>
+      </div>
+      <h2>{count}</h2>
     </div>
   );
 }
